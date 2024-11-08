c3bdb09c43a85d5bbf7f30a022dd47959d4a060e
TSOlami <tijani.saheed@yahoo.com>
2024-11-08 15:23:26 +0100

    Add ARIA_BASE_URL to Config for environment variable management

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 
 class Config:
+    ARIA_BASE_URL=os.environ.get('ARIA_BASE_URL')
     ARIA_API_KEY=os.environ.get('ARIA_API_KEY')
     ALLEGRO_API_KEY=os.environ.get('ALLEGRO_API_KEY')
     OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
